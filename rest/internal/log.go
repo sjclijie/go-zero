1df1d6197c849ffbfd39c29e6a9b116fc815311d
李杰(jet.li) <jet.li@huolala.cn>
2024-03-07 15:50:03 +0800

    upgrade redis-go to v9

--- rest/internal/log.go
+++ rest/internal/log.go
@@ -49,11 +49,11 @@ func (lc *LogCollector) takeAll() []string {
 }
 
 func Error(r *http.Request, v ...interface{}) {
-	logx.ErrorCaller(1, format(r, v...))
+	logx.Error(format(r, v...))
 }
 
 func Errorf(r *http.Request, format string, v ...interface{}) {
-	logx.ErrorCaller(1, formatf(r, format, v...))
+	logx.Error(formatf(r, format, v...))
 }
 
 func Info(r *http.Request, v ...interface{}) {
